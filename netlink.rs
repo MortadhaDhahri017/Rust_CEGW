17392dc4c5ea08aa18d8503e4b70b696bbc63749
MortadhaDhahri17 <122993128+MortadhaDhahri17@users.noreply.github.com>
2024-04-26 15:50:13 +0200

    Update netlink.rs

--- netlink.rs
+++ netlink.rs
@@ -1,6 +1,13 @@
+
+
 use crate::bindings::*;
+use crate::vdev::NetDevice ; 
 
 
+
+///Overrides rust's ethhdr struct in C
+
+pub struct Ethhdr(*mut bindings::ethhdr) ; 
 
 /// Overrides rust's SkBuff struct for our own module
 pub struct SkBuff(*mut bindings::sk_buff);
@@ -107,8 +114,167 @@ impl SkBuff {
     pub fn genlmsg_end(&self,hdr : *mut core::ffi::c_void ) {
         unsafe { bindings::genlmsg_end(self.0, unsafe{hdr}) }  
     }
+
+    ///   The function skb_set_transport_header is a part of
+    ///   the Linux kernel's socket buffer (skb) API.
+    ///   It is used to set the transport header of a socket buffer.
+
+    pub fn skb_set_transport_header(&self , _offset :  core::ffi::c_int) {
+        unsafe {
+            //self.skb_set_transport_header(_offset, _)
+            bindings::skb_set_transport_header(self.0 ,_offset)
+        }
+    } 
+
+    /// skb_set_network_header
+    /// Sets the network header of a `sk_buff` struct.
+    ///
+    /// This function is a helper function for `rust_helper_skb_set_network_header` that contains the actual implementation.
+    /// It sets the `network_header` field of the `sk_buff` struct to the given value, and then copies the first 14 bytes of the struct to the new location of the network header.
+    ///
+    /// Note that this function assumes that the `sk_buff` struct has a particular layout and that the network header is located at a fixed _offset.
+    /// In practice, you should use the `sk_buff` API provided by the Linux kernel to safely manipulate `sk_buff` structs.
+    ///
+    /// # Safety
+    ///
+    /// This function takes a raw pointer to a `sk_buff` struct, which can be dangerous if the pointer is invalid or dangling.
+    /// You should ensure that the pointer is valid and points to a properly initialized `sk_buff` struct before calling this function.
+    ///
+    /// # Arguments
+    ///
+    /// * `skb` - A raw pointer to a `sk_buff` struct.
+    /// * `_offset` - The value to set the network header to.
+
+    pub fn skb_set_network_header(&self , _offset :  core::ffi::c_int) {
+        unsafe {
+            //self.skb_set_network_header(_offset, _)
+            bindings::skb_set_network_header(self.0 ,_offset)
+        }
+    } 
+
+    /// skb_set_mac_header
+    /// Sets the MAC header of a `sk_buff` struct.
+    ///
+    /// This function is a wrapper around the `skb_set_mac_header` function from the Linux kernel's networking stack.
+    /// It sets the `mac_header` field of the `sk_buff` struct to the given value, and then updates the length of the packet.
+    ///
+    /// Note that this function assumes that the `sk_buff` struct has a particular layout and that the MAC header is located at a fixed _offset.
+    /// In practice, you should use the `sk_buff` API provided by the Linux kernel to safely manipulate `sk_buff` structs.
+    ///
+    /// # Safety
+    ///
+    /// This function takes a raw pointer to a `sk_buff` struct, which can be dangerous if the pointer is invalid or dangling.
+    /// You should ensure that the pointer is valid and points to a properly initialized `sk_buff` struct before calling this function.
+    ///
+    /// # Arguments
+    ///
+    /// * `skb` - A raw pointer to a `sk_buff` struct.
+    /// * `value` - The value to set the MAC header to.
     
+    pub fn skb_set_mac_header(&self , _offset :  core::ffi::c_int) {
+        unsafe {
+            //self.skb_set_mac_header(_offset, _)
+            bindings::skb_set_mac_header(self.0 ,_offset)
+        }
+    } 
+
+    ///    skb_reserve - adjust headroom
+    ///    @skb: buffer to alter
+    ///    @len: bytes to move
+    ///
+    ///    Increase the headroom of an empty &sk_buff by reducing the tail
+    ///    room. This is only allowed for an empty buffer.
     
+    pub fn skb_reserve(&self , len: i32) {
+        unsafe {
+            bindings::skb_reserve(self.0,len)
+        }
+    }
+
+
+    /// skb_network_header
+    /// The function calculates the address by adding the network_header field's value to the head field's address.
+    /// The network_header field contains the _offset of the network header from the start of the socket buffer, 
+    /// and the head field points to the start of the socket buffer's data area.
+    /// This function is useful when you need to directly access or manipulate 
+    /// the network header of a packet stored in a socket buffer.
+
+
+    pub fn skb_network_header(&self) ->*mut core::ffi::c_uchar{
+        unsafe {
+            bindings::skb_network_header(self.0)
+        }
+    }
+
+    /// skb_tail_pointer 
+    /// Returns a pointer to the tail of the socket buffer (sk_buff)
+    /// The tail pointer marks the end of the valid data in the socket buffer
+
+    pub fn skb_tail_pointer(&self)->*mut core::ffi::c_uchar{
+        unsafe {
+            bindings::skb_tail_pointer(self.0)
+        }
+    }
+    
+    ///	skb_push - add data to the start of a buffer
+    ///	@skb: buffer to use
+    ///	@len: amount of data to add
+    ///
+    ///	This function extends the used data area of the buffer at the buffer
+    ///	start. If this would exceed the total buffer headroom the kernel will
+    ///	panic. A pointer to the first byte of the extra data is returned.
+    
+    pub fn skb_push(&self, len: core::ffi::c_uint) -> *mut core::ffi::c_void{
+        unsafe {
+            bindings::skb_push(self.0 ,len)
+        }
+    }
+
+    ///
+    ///	skb_copy_expand	-	copy and expand sk_buff
+    ///	@skb: buffer to copy
+    ///	@newheadroom: new free bytes at head
+    ///	@newtailroom: new free bytes at tail
+    ///	@gfp_mask: allocation priority
+    ///
+    ///	Make a copy of both an &sk_buff and its data and while doing so
+    ///	allocate additional space.
+    ///
+    ///	This is used when the caller wishes to modify the data and needs a
+    ///	private copy of the data to alter as well as more space for new fields.
+    ///	Returns %NULL on failure or the pointer to the buffer
+    ///	on success. The returned buffer has a reference count of 1.
+    ///
+    ///	You must pass %GFP_ATOMIC as the allocation priority if this function
+    ///	is called from an interrupt.
+
+    pub fn skb_copy_expand(
+        skb: *const sk_buff,
+        newheadroom: core::ffi::c_int,
+        newtailroom: core::ffi::c_int,
+        priority: gfp_t,
+    ) -> SkBuff {
+        unsafe {
+            SkBuff(bindings::skb_copy_expand(skb,newheadroom,newtailroom,priority)) 
+        }
+    }
+
+    /// No docs as for now 
+
+    pub fn ip_hdrlen(&self)->u32 {
+        unsafe {
+            bindings::ip_hdrlen(self.0) 
+        }
+    }
+
+    //// No docs as for now 
+    pub fn eth_hdr(&self)->Ethhdr {
+        unsafe{
+            Ethhdr(bindings::eth_hdr(self.0))
+        }
+    }
+
+
 }
 
 /// Sock is a struct that wraps C's sruct sock using
@@ -154,7 +320,3 @@ impl Nlattr {
         unsafe { bindings::nla_data(self.0) }
 }
 }
-
-
-
-
