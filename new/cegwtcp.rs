58176ad0d5f017777a33dc7ac9afa6ba8493c18b
Mortadha Dhahri <122993128+MortadhaDhahri017@users.noreply.github.com>
2024-05-22 14:02:04 +0100

    Update cegwtcp.rs

--- new/cegwtcp.rs
+++ new/cegwtcp.rs
@@ -112,42 +112,76 @@ impl EthFrame {
         bytes
     }
 
-
+  
     
-
-    pub fn deserialize_ethernet(buffer: &[u8]) -> Option<Self> {
-            // Extract individual field slices using nom library
-            let (dst_mac, rest) = buffer.split_at(6);
-            let (src_mac, rest) = rest.split_at(6);
+        pub fn deserialize_ethernet(buffer:&[u8]) -> Option<EthFrame> {
+            let (dst, rest) = buffer.split_at(6);
+            let (src, rest) = rest.split_at(6);
             let (ethertype, rest) = rest.split_at(2);
-            let (data_ipv4_bytes, rest) = rest.split_at(46);
-            let (fsc,_)=rest.split_at(4) ; 
+            let (iphdr_bytes, rest) = rest.split_at(21);
+            let (tcphdr_bytes, rest) = rest.split_at(21);
+            let (data_eth, rest) = rest.split_at(4);
+            let (fsc, _) = rest.split_at(4);
     
-            let mut dst_arr = [0; 6];
-            dst_arr.copy_from_slice(dst_mac);
     
-            let mut eth_arr = [0; 2];
-            eth_arr.copy_from_slice(ethertype);
-    
-            let mut src_arr = [0; 6];
-            src_arr.copy_from_slice(src_mac);
-    
-            // Deserialize individual fields
-            let data_ipv4 = Ipv4Frame::from_bytes(data_ipv4_bytes)?;
-
-
-            let mut fsc_arr = [0; 4];
+            let mut ethertype_arr = [0; 2];
+            let mut fsc_arr = [0; 4] ; 
+            ethertype_arr.copy_from_slice(ethertype);
             fsc_arr.copy_from_slice(fsc);
-            
-            let fsc = u32::from_be_bytes(fsc_arr);
     
-            Some(Self {
-                dst: dst_arr,
-                src: src_arr,
-                ethertype: EtherType(u16::from_be_bytes(eth_arr)),
-                data_ipv4,
-                fsc, // You'll need to adjust this based on your specific requirements
-            })
+    
+    
+            let src_vec = src.to_owned();
+            let src_v1=vec_to_array6(&src_vec) ;  
+            let dst_vec = dst.to_owned();
+            let dst_v1=vec_to_array6(&dst_vec) ; 
+            let ethertype = u16::from_be_bytes(ethertype_arr);
+            let iphdr = Ipv4Header::deserialize_ip_header(iphdr_bytes)?;
+            let tcphdr = TcpHeader::deserialize_tcp_header(tcphdr_bytes)?;
+            let data_eth_vec = data_eth.to_owned();
+            let data_eth_v1=vec_to_array4(&data_eth_vec) ; 
+            let fsc = u32::from_be_bytes(fsc_arr);
+            Some(EthFrame { 
+                dst: dst_v1.unwrap(),
+                src: src_v1.unwrap(),
+                ethertype: EtherType(ethertype),
+                data_ipv4: Ipv4Frame{
+                    header: iphdr,
+                    data_tcp: TCP_Frame{
+                        header: tcphdr,
+                        data: data_eth_v1.unwrap(),
+                    },
+                },
+                fsc: fsc,
+                 })
+     }
+}
+  
+    fn vec_to_array6(slice_data: &[u8]) -> Result<[u8; 6], &'static str> {
+        // Check if the slice has exactly 4 elements
+        if slice_data.len() != 6 {
+            return Err("Error: Slice must contain exactly 6 elements");
+        }
+    
+        // Safely convert the slice to a fixed-size array
+        unsafe {
+            // This is safe because we checked the slice length beforehand
+            let array: [u8; 6] = *(slice_data.as_ptr() as *const [u8; 6]);
+            Ok(array)
+        }
+    }
+
+    fn vec_to_array4(slice_data: &[u8]) -> Result<[u8; 4], &'static str> {
+        // Check if the slice has exactly 4 elements
+        if slice_data.len() != 4 {
+            return Err("Error: Slice must contain exactly 6 elements");
+        }
+    
+        // Safely convert the slice to a fixed-size array
+        unsafe {
+            // This is safe because we checked the slice length beforehand
+            let array: [u8; 4] = *(slice_data.as_ptr() as *const [u8; 4]);
+            Ok(array)
         }
     }
 
